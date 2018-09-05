83a9a4f24a98edb91aded9bab5507ca3c0e5cbe9
hravera <42938794+hravera@users.noreply.github.com>
2018-09-05 14:24:45 +0200

    Test build automatic

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World today!";
+    private final String message = "Hello World today and tomorrow!";
 
     public App() {}
 
